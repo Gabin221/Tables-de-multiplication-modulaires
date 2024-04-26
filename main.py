755e5069f66a786dd19e98afdd48ee412a75199b
Gabin221 <serrurot19@gmail.com>
2024-04-26 14:27:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def hello():
-	print("Hello World!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def tableModulaire(table, modulo):
+	plt.figure()
+ 
 
 
 if __name__ == "__main__":
 	table = input("Quelle table allons nous tracer ?")
 	modulo = input("Modulo ?")
-	hello()
+	tableModulaire(table, modulo)
