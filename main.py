f458b02b0b56a1e8c704b4b5f839fb29d26bbaaf
Gabin221 <serrurot19@gmail.com>
2024-04-26 14:41:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ import numpy as np
 
 
 def tableModulaire(table, modulo):
+	angles = np.linspace(0, 2*np.pi, 1000)
+	points = np.linspace(0, 2*np.pi, modulo)
+
+	cosinusAngles = np.cos(angles)
+	sinusAngles = np.sin(angles)
+
+	cosinusPoints = np.cos(points + np.pi/2)
+	sinusPoints = np.sin(points + np.pi/2)
+
 	plt.figure()
- 
+	plt.plot(cosinusAngles, sinusAngles)
+	plt.plot(cosinusPoints, sinusPoints, 'o')
+	plt.axis('equal')
+	plt.show()
 
 
 if __name__ == "__main__":
-	table = input("Quelle table allons nous tracer ?")
-	modulo = input("Modulo ?")
+	# table = input("Quelle table allons nous tracer ? ")
+	# modulo = input("Modulo ? ")
+	table = 2
+	modulo = 10
 	tableModulaire(table, modulo)
